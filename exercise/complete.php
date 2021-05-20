a2a270eb39963943446c711a2a34119c43aa4d50
jiae yoon <68904755+jiaeYoon@users.noreply.github.com>
2021-05-20 22:31:26 +0900

    $_SESSION으로 id값 찾아서 INSERT 완료

--- exercise/complete.php
+++ exercise/complete.php
@@ -2,11 +2,14 @@
   $conn = mysqli_connect("us-cdbr-east-03.cleardb.com", "bb0e75dfd58ff1", "73c3064a", "heroku_1189b05c9eafafd");
   
   /* 세션에 저장해둔 사용자 아이디(login_id)값 가져오기 */
-  $id = $_SESSION['userId'];
-  print_r($_SESSION);
-
+  session_start();
+  print_r($_SESSION['userId']);
+  $id = $_SESSION;
+  $id = implode("", $id);
+  echo gettype($id);
+  
   /* challenge 테이블에 사용자의 id값, 날짜 삽입 */
-  $sql = "insert into challenge(id, c_date) values((SELECT user_info.id from user_info where user_info.login_id='{$id}'), now());";
+  $sql = "INSERT INTO challenge(id, c_date) VALUES($id, now())";
   $result = mysqli_query($conn, $sql);
 
   if($result === false){
