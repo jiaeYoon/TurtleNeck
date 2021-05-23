c1f6980f8f7908fa7997b74638cf41c7ee269e06
jiae yoon <68904755+jiaeYoon@users.noreply.github.com>
2021-05-23 10:16:18 +0900

    edit INSERT statement

--- signin/signupProcess.php
+++ signin/signupProcess.php
@@ -1,12 +1,11 @@
 <?php
-    phpinfo();
     $conn = mysqli_connect("us-cdbr-east-03.cleardb.com", "bb0e75dfd58ff1", "73c3064a", "heroku_1189b05c9eafafd");
-    $hashedPassword = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $hashedPassword = password_hash($_POST['PW'], PASSWORD_DEFAULT);
     echo $hashedPassword;
     $sql = "
-        INSERT INTO user
-        (email, password, name)
-        VALUES('{$_POST['email']}', '{$hashedPassword}', '{$_POST['name']}'
+        INSERT INTO user_info
+        (login_id, pw, u_name)
+        VALUES('{$_POST['ID']}', '{$hashedPassword}', '{$_POST['name']}'
         )";
     echo $sql;
     $result = mysqli_query($conn, $sql);
@@ -16,15 +15,16 @@
         echo mysqli_error($conn);
 ?>
     <script>
-        location.href = location.href;
+        location.href = "index.html";
     </script>
 <?php
     } else {
 ?>
     <script>
         alert("회원가입이 완료되었습니다");
-        //location.href = "mainpage.html"; //로그인창 또는 메인페이지로 이동하기
+        location.href = "main.html";
      </script>
  <?php
       }
 ?>
+
