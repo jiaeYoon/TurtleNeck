126acaebf0c4899d7770970c5b16b3042b50a20a
minsunkim <kmsun0524@gmail.com>
2021-07-21 22:03:39 +0900

    main page HTML, CSS 완료

--- main/main.js
+++ main/main.js
@@ -1,2 +1,20 @@
-//w진짜 main 관련
+'use strict';
 
+/* header buttons(profile, menu bar) */
+const toggleBtn = document.querySelector('#toggleBtn');
+const profileBtn = document.querySelector('#profile');
+
+const menu = document.querySelector('.navbar__menu');
+const hidden_box = document.querySelector('.hidden__bg');
+const tooltip = document.querySelector('.tooltip');
+
+toggleBtn.addEventListener('click', () => {
+  menu.classList.toggle('active');
+  hidden_box.classList.toggle('active');
+  toggleBtn.classList.toggle('active');
+  profileBtn.classList.toggle('active');
+});
+
+profileBtn.addEventListener('click', () => {
+  tooltip.classList.toggle('active');
+});
